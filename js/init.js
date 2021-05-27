e8c4e561f24b084bdbe75581d8dd7442190cd74a
jpinaa1400 <84769836+jpinaa1400@users.noreply.github.com>
2021-05-27 03:05:53 -0500

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,9 @@
+firebase.initializeApp({
+  apiKey: "AIzaSyDa1pqGc-0vFtPreMOUhB81YzTFzyQW6DM",
+  authDomain: "jpinaa1400.firebaseapp.com",
+  projectId: "jpinaa1400",
+  storageBucket: "jpinaa1400.appspot.com",
+  messagingSenderId: "318190814359",
+  appId: "1:318190814359:web:3c7415582df8c53375d5d2",
+  measurementId: "G-Q5YH3GX5NF"
+}):
